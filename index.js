7acb4d7e6be1cba2c3a63b4b0d1c61d017332d7b
Seher Saygılı <sehersygl111@gmail.com>
2024-01-20 14:33:43 +0300

    dark themes uploaded

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggler = document.querySelector(".theme-toggler");
+
+    if (themeToggler) {
+        themeToggler.addEventListener('click', () => {
+            document.body.classList.toggle('dark-theme-variables');
+            
+            // Toggle the "active" class between the two spans
+            const lightModeIcon = themeToggler.querySelector('.material-symbols-outlined.active');
+            const darkModeIcon = themeToggler.querySelector('.material-symbols-outlined:not(.active)');
+            
+            lightModeIcon.classList.toggle('active');
+            darkModeIcon.classList.toggle('active');
+        });
+    } else {
+        console.error('Theme toggler element not found.');
+    }
+});
